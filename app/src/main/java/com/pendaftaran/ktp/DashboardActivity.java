5f3f6173f9a7ed405f188b5746d43b5a4c807598
bagasbest <45277197+bagasbest@users.noreply.github.com>
2021-10-07 20:16:46 +0700

    fdashboard

---
+++ app/src/main/java/com/pendaftaran/ktp/DashboardActivity.java
@@ -0,0 +1,41 @@
+package com.pendaftaran.ktp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.pendaftaran.ktp.databinding.ActivityDashboardBinding;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private ActivityDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
--- app/src/main/java/com/pendaftaran/ktp/MainActivity.java
+++ app/src/main/java/com/pendaftaran/ktp/MainActivity.java
@@ -2,13 +2,37 @@ package com.pendaftaran.ktp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.bumptech.glide.Glide;
+import com.pendaftaran.ktp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Glide.with(this).load(R.drawable.logo).into(binding.imageView);
+
+        // delay 4 detik sebelum masuk ke dashboard
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            finish();
+        }, 4000);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
